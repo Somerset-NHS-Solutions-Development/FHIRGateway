aa81322c8bed3c5d944ea4bf44589e5e8cd7803c
Frazer Smith <frazer.smith@ydh.nhs.uk>
2020-03-10 09:10:51 +0000

    refactor(handlers): prefer const over vars
    See https://eslint.org/docs/rules/prefer-const

--- src/handlers/login.js
+++ src/handlers/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router()
-var request = require("request");
+const request = require("request");
 
 const jwt = require('jsonwebtoken')
 const verifyToken = require('./verify-token');
@@ -12,13 +12,13 @@ router.post('/login', (req, res) => {
     const username = user.username;
     const password = user.password;
     // console.log(user);
-    var options = {
+    const options = {
         method: 'POST',
         url: (process.env.openIDDirectAccessEnpoint),
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
         form: {
-            username: username, 
-            password: password, 
+            username, 
+            password, 
             client_id: (process.env.openIDClientID),
             grant_type: 'password',
 			client_secret: (process.env.openIDClientSecret)
@@ -28,8 +28,8 @@ router.post('/login', (req, res) => {
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
 
-        var json = (JSON.parse(body));
-        var fred = jwt.decode(json.access_token);       
+        const json = (JSON.parse(body));
+        const fred = jwt.decode(json.access_token);       
             
 		// console.log(fred);
 		
